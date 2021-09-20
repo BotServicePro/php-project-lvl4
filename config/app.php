d69c9a1910854ef86a67427b4dbb22999f670543
akarakin <bulik889@gmail.com>
2021-09-20 12:04:02 +0300

    Rollbar added

--- config/app.php
+++ config/app.php
@@ -162,6 +162,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        Rollbar\Laravel\RollbarServiceProvider::class,
+
         /*
          * Package Service Providers...
          */
