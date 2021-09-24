9b98ae8ab1534004b33ad84b5811b942b8738495
akarakin <bulik889@gmail.com>
2021-09-24 23:22:56 +0300

    Edited

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskStatusController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,8 +18,6 @@ Route::get('/', function () {
     return view('index');
 })->name('index');
 
-//Route::get('/dashboard', function () {
-//    return view('dashboard');
-//})->middleware(['auth'])->name('dashboard');
+Route::resource('task_statuses', TaskStatusController::class)->middleware('auth');
 
 require __DIR__.'/auth.php';
