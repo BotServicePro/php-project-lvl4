6c2761ed38acb53bdc568811afb4d438b7c251b2
akarakin <bulik889@gmail.com>
2021-09-27 21:00:37 +0300

    Added new tests

--- tests/Feature/TaskStatuseCRUDTest.php
+++ tests/Feature/TaskStatuseCRUDTest.php
@@ -2,21 +2,75 @@
 
 namespace Tests\Feature;
 
+use App\Models\TaskStatus;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\URL;
 use Tests\TestCase;
 
 class TaskStatuseCRUDTest extends TestCase
 {
+    /** @var int */
+    private $id;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $statuseNames = ['новый', 'в работе', 'на тестировании', 'завершен'];
+        foreach ($statuseNames as $name) {
+            $status = new TaskStatus();
+            $status->name = $name;
+            $status->save();
+        }
+//        $tasks = TaskStatus::all();
+//        foreach ($tasks as $task) {
+//            var_dump($task->id);
+//        }
+    }
+
+
     /**
      * A basic feature test example.
      *
      * @return void
      */
-    public function test_example()
+    public function testTaskStatusePages()
     {
-        $response = $this->get('/');
-
+        $response = $this->get('/task_statuses');
         $response->assertStatus(200);
+    }
+
+    // авторизация
+    protected function signIn($user = null)
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        return $this;
+    }
+
+    public function testTaskStatuseAdd()
+    {
+        $this->signIn();
+        $response = $this->get('/task_statuses/create'); // вход на страницу авторизованным юзером
+        $response->assertStatus(200);
+
+        // формируем даные для записи
+        $taskData = ['name' => 'Тестовый статус'];
+
+        // формируем запрос
+        $response = $this->post(route('task_statuses.store'), $taskData);
+        $response->assertSessionHasNoErrors();
+
+        // проверяем редирект после успешного добавления нового статуса
+        $response->assertRedirect(route('task_statuses.index'));
+
+        // проверяем в базе наличие нового статуса
+        $this->assertDatabaseHas('task_statuses', $taskData);
+    }
+    public function testTaskStatuseEdit()
+    {
+        //
     }
 }
