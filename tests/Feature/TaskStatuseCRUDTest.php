bb8b33e07ca2364a2bd0a36c0a701432485d4fcd
akarakin <bulik889@gmail.com>
2021-09-28 14:22:06 +0300

    Added tests for create/update/delete methods

--- tests/Feature/TaskStatuseCRUDTest.php
+++ tests/Feature/TaskStatuseCRUDTest.php
@@ -24,10 +24,8 @@ class TaskStatuseCRUDTest extends TestCase
             $status->name = $name;
             $status->save();
         }
-//        $tasks = TaskStatus::all();
-//        foreach ($tasks as $task) {
-//            var_dump($task->id);
-//        }
+
+        $this->id = TaskStatus::where('id', '=', 1)->get();
     }
 
 
@@ -52,6 +50,10 @@ class TaskStatuseCRUDTest extends TestCase
 
     public function testTaskStatuseAdd()
     {
+        $response = $this->get('/task_statuses/create'); // вход на страницу авторизованным юзером
+        $response->assertStatus(403); // в случае если НЕ авторизованы
+
+
         $this->signIn();
         $response = $this->get('/task_statuses/create'); // вход на страницу авторизованным юзером
         $response->assertStatus(200);
@@ -71,6 +73,54 @@ class TaskStatuseCRUDTest extends TestCase
     }
     public function testTaskStatuseEdit()
     {
-        //
+        foreach ($this->id as $item) {
+            $id = $item->id;
+        }
+
+        $response = $this->get("/task_statuses/{$id}/edit"); // вход на страницу авторизованным юзером
+        $response->assertStatus(403); // в случае если НЕ авторизованы
+
+
+        $this->signIn();
+
+        $response = $this->get("/task_statuses/{$id}/edit"); // вход на страницу авторизованным юзером
+        $response->assertStatus(200);
+
+        // формируем даные для записи
+        $taskData = [
+            'name' => 'ОБНОВЛЁННЫЙ статус',
+        ];
+
+        // формируем запрос
+        //$response = $this->patch(route('task_statuses.update'), $taskData);
+        $response = $this->patch("task_statuses/{$id}", $taskData);
+        $response->assertSessionHasNoErrors();
+
+        foreach(TaskStatus::where('id', '=', 1)->get() as $item) {
+            $result = $item;
+        }
+
+        $this->assertEquals($taskData['name'], $result->name);
+        $this->assertEquals(1, $result->id);
+    }
+
+    public function testTaskStatuseDelete()
+    {
+        foreach ($this->id as $item) {
+            $id = $item->id;
+        }
+
+        $response = $this->delete("task_statuses/{$id}");
+        $response->assertStatus(403);
+
+        $this->signIn();
+        $response = $this->delete("task_statuses/{$id}");
+        $response->assertStatus(302);
+        $response->assertRedirect(route('task_statuses.index'));
+
+
+        $deletedTask = TaskStatus::where('id', '=', 1)->get()->toArray();
+        $this->assertEquals([], $deletedTask);
+
     }
 }
