9a2c18c17e51e1dffcdf3f98fc312df0fd4d72dd
akarakin <bulik889@gmail.com>
2021-10-07 11:43:56 +0300

    Seeder edited

--- database/seeders/TaskSeeder.php
+++ database/seeders/TaskSeeder.php
@@ -15,12 +15,22 @@ class TaskSeeder extends Seeder
      */
     public function run()
     {
+
+        // сделать через цикл, данные задачи поместить в массив
         $newTask = new Task();
         $newTask->name = 'Самая первая задача';
         $newTask->description = 'Очень важная';
         $newTask->status_id = 1;
         $newTask->assigned_to_id = 1;
         $newTask->created_by_id = 1;
+        $newTask->save();
+
+        $newTask = new Task();
+        $newTask->name = 'Самая ВТОРАЯ задача';
+        $newTask->description = 'Ну так себе по важности';
+        $newTask->status_id = 2;
+        $newTask->assigned_to_id = 2;
+        $newTask->created_by_id = 2;
         $newTask->save();
     }
 }
