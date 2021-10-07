b1b14a9b27da2145642495e44dfa992527ed63d8
akarakin <bulik889@gmail.com>
2021-10-07 21:34:16 +0300

    Added show page

---
+++ resources/views/taskPages/show.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('title', 'Просмотр задачи')
+
+@include('flash::message')
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@section('content')
+    <main class="container py-4">
+        <h2>Просмотр задачи: {{ $taskData->name }}
+            @if(Auth::check())
+            [<a href="/tasks/{{ $taskData->id }}/edit"><font style="color: #e3342f">Редактировать</font></a>]
+            @endif
+        </h2>
+        <br>
+        <div>
+            Имя: {{ $taskData->name }}
+        </div>
+        <br>
+        <div>
+            Описание: {{ $taskData->description }}
+        </div>
+        <br>
+        <div>
+            Статус: {{ $statusData->name }}
+        </div>
+        <br>
+        <div>
+            Метки:
+        </div>
+    </main>
+@endsection
