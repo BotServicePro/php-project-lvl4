5945455452d6d52aad5de1b57a959636a552d147
akarakin <bulik889@gmail.com>
2021-10-07 21:31:02 +0300

    Edited

--- resources/views/taskPages/edit.blade.php
+++ resources/views/taskPages/edit.blade.php
@@ -18,24 +18,23 @@
     <main class="container py-4">
         <h2>Редактировать задачу</h2>
         {{ Form::token() }}
-        {{ Form::model($task, ['url' => "taskы/{$task['id']}", 'method' => 'PATCH']) }}
+        {{ Form::model($task, ['url' => "tasks/{$task['id']}", 'method' => 'PATCH']) }}
         {{ Form::label('name', 'Имя') }}<font style="color: #e3342f">*</font><br>
         {{ Form::text('name', $task['name']) }}<br><br>
 
-        {{ Form::label('description', 'Описание') }}<font style="color: #e3342f">*</font><br>
+        {{ Form::hidden('created_by_id', $task['created_by_id']) }}
+
+        {{ Form::label('description', 'Описание') }}<br>
         {{ Form::textarea('description', $task['description']) }}<br><br>
 
         {{ Form::label('status_id', 'Статус') }}<font style="color: #e3342f">*</font><br>
-        {{  Form::select('status_id', $task, null, ['placeholder' => '---']) }}<br><br>
+        {{  Form::select('status_id', $taskStatusesList, null, ['placeholder' => '---']) }}<br><br>
 
-        {{ Form::label('assigned_to_id', 'Исполнитель') }}<font style="color: #e3342f">*</font><br>
-        {{  Form::select('assigned_to_id', $task, null, ['placeholder' => '---']) }}<br><br>
+        {{ Form::label('assigned_to_id', 'Исполнитель') }}<br>
+        {{  Form::select('assigned_to_id', $usersList, null, ['placeholder' => '---']) }}<br><br>
 
 {{--        {{ Form::label('assigned_to_id', 'Метки') }}<font style="color: #e3342f">*</font><br>--}}
 {{--        {{  Form::select('assigned_to_id', $task, null, ['placeholder' => '---']) }}<br><br>--}}
-
-
-
 
         {{ Form::submit('Обновить') }}<br>
     </main>
