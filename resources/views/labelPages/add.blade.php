d1d6e646062b6cf78a5b139e9dd5bcedbe2bb167
akarakin <bulik889@gmail.com>
2021-10-11 13:09:47 +0300

    Added file

---
+++ resources/views/labelPages/add.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('title', 'Создать метку')
+
+{{--@include('flash::message')--}}
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+@section('content')
+    <main class="container py-4">
+        <h2>Создать метку</h2>
+        {{ Form::token() }}
+        {{ Form::model($label, ['url' => route('labels.store'), 'method' => 'POST']) }}
+        {{ Form::label('name', 'Имя') }}<font style="color: #e3342f">*</font><br>
+        {{ Form::text('name') }}<br><br>
+
+        {{ Form::label('description', 'Описание') }}<br>
+        {{ Form::textarea('description') }}<br><br>
+
+        {{ Form::submit('Создать') }}<br>
+    </main>
+@endsection
