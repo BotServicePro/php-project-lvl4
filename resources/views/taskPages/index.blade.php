e9d9892c29003440ff766c412c9fb79dc4c8f40a
akarakin <bulik889@gmail.com>
2021-10-12 12:58:03 +0300

    Added filter form

--- resources/views/taskPages/index.blade.php
+++ resources/views/taskPages/index.blade.php
@@ -17,42 +17,37 @@
     <main class="container py-4">
         <h2>Задачи</h2>
         <br>
-        @if(Auth::check())
-            <a href="{{ route('tasks.create') }}" class="btn btn-primary">Создать задачу</a>
-        @endif
 
+        <div class="d-flex">
+            <div>
+                <form method="GET" action="/tasks" accept-charset="UTF-8">
+                    <select class="mr-2" name="filter[status_id]">
+                        <option value="">Статус</option>
+                        <option value="10">kookok</option>
+                        <option value="126">status777</option>
+                    </select>
+                    <select name="filter[created_by_id]">
+                        <option value="">Автор</option>
 
+                        <option value="1">Alex</option><option value="2">feycot</option>
+                        <option value="133">Виталий Кудрявцев</option>
+                        <option value="135">Иван Князев</option>
+                        <option value="134">Виталий Кудрявцев</option>
+                    </select>
+                    <select  name="filter[assigned_to_id]">
+                        <option selected="selected" value="">Исполнитель</option>
 
-{{--        <div class="d-flex">--}}
-{{--            <div>--}}
-{{--                <form method="GET" action="https://php-l4-task-manager.herokuapp.com/tasks" accept-charset="UTF-8">--}}
-{{--                    <select class="form-control mr-2" name="filter[status_id]">--}}
-{{--                        <option value="">Статус</option>--}}
-{{--                        <option value="10">kookok</option>--}}
-{{--                        <option value="126">status777</option>--}}
-{{--                    </select>--}}
-{{--                    <select class="form-control mr-2" name="filter[created_by_id]">--}}
-{{--                        <option value="">Автор</option>--}}
-
-{{--                        <option value="1">Alex</option><option value="2">feycot</option>--}}
-{{--                        <option value="133">Виталий Кудрявцев</option>--}}
-{{--                        <option value="135">Иван Князев</option>--}}
-{{--                        <option value="134">Виталий Кудрявцев</option>--}}
-{{--                    </select>--}}
-{{--                    <select class="form-control mr-2" name="filter[assigned_to_id]">--}}
-{{--                        <option selected="selected" value="">Исполнитель</option>--}}
-
-{{--                        <option value="1">Alex</option><option value="2">feycot</option>--}}
-{{--                        <option value="152">ddf</option><option value="151">Sem</option>--}}
-{{--                        <option value="150">testerSuper</option>--}}
-{{--                    </select>--}}
-{{--                    <input class="btn btn-outline-primary mr-2" type="submit" value="Применить">--}}
-{{--                </form>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-
-
-
+                        <option value="1">Alex</option><option value="2">feycot</option>
+                        <option value="152">ddf</option><option value="151">Sem</option>
+                        <option value="150">testerSuper</option>
+                    </select>
+                    <input class="btn btn-outline-primary mr-2" type="submit" value="Применить">
+                </form>
+            </div>
+            @if(Auth::check())
+                <a href="{{ route('tasks.create') }}" class="btn btn-primary ml-auto">Создать задачу</a>
+            @endif
+        </div>
 
         <table class="table mt-2">
             <thead>
