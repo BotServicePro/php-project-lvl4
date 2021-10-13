762e9a276f22a05ec8ebeb10095e11aa2dcea311
akarakin <bulik889@gmail.com>
2021-10-13 13:03:11 +0300

    Added language files

---
+++ resources/lang/en/interface.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | INTERFACE Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    // :date :min :max :value :values :other :digits :size
+
+    'siteName' => 'Task manager',
+    'tasks' => 'Tasks',
+    'statuses' => 'Statuses',
+    'labels' => 'Labels',
+    'label' => 'Lable',
+
+
+    'status' => 'Status',
+    'author' => 'Author',
+    'name' => 'Name',
+    'employee' => 'Employee',
+    'createDate' => 'Created at',
+    'updateDate' => 'Updated at',
+    'settings' => 'Settings',
+    'delete' => 'Delete',
+    'update' => 'Update',
+
+    'showTask' => 'Show task',
+
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'createTask' => 'Create task',
+    'createStatus' => 'Create status',
+    'createLabel' => 'Create label',
+    'editTask' => 'Edit task',
+    'editStatus' => 'Edit status',
+    'editLabel' => 'Edit label',
+
+    'description' => 'Description',
+    'apply' => 'Apply',
+
+    'logout' => 'Logout',
+    'login' => 'Login',
+    'register' => 'Register',
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [],
+
+];
---
+++ resources/lang/ru/interface.php
@@ -0,0 +1,67 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | INTERFACE Language Lines
+    |--------------------------------------------------------------------------
+    */
+
+    // :date :min :max :value :values :other :digits :size
+
+    'siteName' => 'Менеджер задач',
+    'tasks' => 'Задачи',
+    'statuses' => 'Статусы',
+    'labels' => 'Метки',
+    'label' => 'Метка',
+
+    'status' => 'Статус',
+    'author' => 'Автор',
+    'name' => 'Имя',
+    'employee' => 'Исполнитель',
+    'createDate' => 'Дата создания',
+    'updateDate' => 'Дата обновления',
+    'settings' => 'Действия',
+    'delete' => 'Уадить',
+    'update' => 'Обновить',
+
+    'create' => 'Создать',
+    'edit' => 'Изменить',
+    'createTask' => 'Создать задачу',
+    'createStatus' => 'Создать статус',
+    'createLabel' => 'Создать метку',
+
+    'showTask' => 'Просмотр задачи',
+
+    'editTask' => 'Изменение задачи',
+    'editStatus' => 'Изменение статуса',
+    'editLabel' => 'Изменение метки',
+
+    'description' => 'Описание',
+    'apply' => 'Применить',
+
+    'logout' => 'Выход',
+    'login' => 'Вход',
+    'register' => 'Регистрация',
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [],
+
+];
