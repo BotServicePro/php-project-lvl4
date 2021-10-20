66c07b4009d7d1a4358d17fc3a48e38f7c1c8df3
akarakin <bulik889@gmail.com>
2021-10-20 14:54:49 +0300

    Edited

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -70,15 +70,15 @@
 </header>
 
 <div class="py-12">
-    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+{{--    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">--}}
 
-        <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
-            <div class="p-6 bg-white border-gray-200">
+{{--        <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">--}}
+{{--            <div class="p-6 bg-white border-gray-200">--}}
                 @include('flash::message')
                 @yield('content')
-            </div>
-        </div>
-    </div>
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    </div>--}}
 </div>
 
 <div class="wrapper flex-grow-1"></div>
