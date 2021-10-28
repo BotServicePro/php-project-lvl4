09077718be4db0d80547ae393e45dfc1d3e71e3d
akarakin <bulik889@gmail.com>
2021-10-28 18:43:41 +0300

    Linter fixed

--- tests/Feature/MainPageTest.php
+++ tests/Feature/MainPageTest.php
@@ -13,7 +13,7 @@ class MainPageTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function testExample()
     {
         $response = $this->get('/');
         $response->assertStatus(200);
