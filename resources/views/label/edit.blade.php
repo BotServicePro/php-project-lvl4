d2a42b72e1a4a086b238920105ec90c699cacf7f
akarakin <bulik889@gmail.com>
2021-11-27 18:25:15 +0200

    changed to fluid interface

--- resources/views/label/edit.blade.php
+++ resources/views/label/edit.blade.php
@@ -6,12 +6,12 @@
     <main class="container py-4">
         <h2>{{ __('interface.editLabel') }}</h2>
         {{ Form::token() }}
-        {{ Form::model($label, ['url' => "labels/{$label['id']}", 'method' => 'PATCH']) }}
+        {{ Form::model($label, ['url' => "labels/{$label->id}", 'method' => 'PATCH']) }}
         {{ Form::label('name', __('interface.name')) }}<font style="color: #e3342f">*</font><br>
-        {{ Form::text('name', $label['name']) }}<br><br>
+        {{ Form::text('name', $label->name) }}<br><br>
 
         {{ Form::label('description', __('interface.description')) }}<br>
-        {{ Form::textarea('description', $label['description']) }}<br><br>
+        {{ Form::textarea('description', $label->description) }}<br><br>
         {{ Form::submit(__('interface.update'), ['class' => 'btn btn-outline-primary mr-2']) }}<br>
     </main>
 @endsection
