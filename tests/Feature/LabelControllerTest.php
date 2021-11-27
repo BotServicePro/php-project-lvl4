8b1ce899909f11632178e8163d13139532814b68
akarakin <bulik889@gmail.com>
2021-11-27 18:46:07 +0200

    edited

--- tests/Feature/LabelControllerTest.php
+++ tests/Feature/LabelControllerTest.php
@@ -13,23 +13,20 @@ class LabelControllerTest extends TestCase
 {
     /** @var int */
     private $id;
+    /**
+     * @var \Illuminate\Database\Eloquent\Collection|\Illuminate\Database\Eloquent\Model
+     */
+    private $user;
 
     protected function setUp(): void
     {
         parent::setUp();
         $totalRecords = 5;
-        User::factory()->count($totalRecords)->create();
+        $this->user = User::factory()->create();
         Label::factory()->count($totalRecords)->create();
         TaskStatus::factory()->count($totalRecords)->create();
         Task::factory()->create();
         $this->id = Label::find(1)->id;
-    }
-
-    protected function signIn(): LabelControllerTest
-    {
-        $user = User::factory()->create()->first();
-        $this->actingAs($user);
-        return $this;
     }
 
     /**
@@ -41,52 +38,30 @@ class LabelControllerTest extends TestCase
     {
         $response = $this->get('/labels');
         $response->assertStatus(200);
-        $response->assertSeeTextInOrder(
-            [
-                'ID',
-                __('interface.name'),
-                __('interface.description'),
-                __('interface.createDate'),
-                ],
-            true
-        );
-        $response->assertDontSeeText(
-            [
-                __('interface.createLabel'),
-                __('interface.settings')],
-            true
-        );
-        $this->signIn();
-        $response = $this->get('/labels');
-        $response->assertSeeTextInOrder(
-            [
-                __('interface.createLabel'),
-                __('interface.settings')],
-            true
-        );
     }
 
-    public function testCreate(): void
+    public function testCreate()
     {
         $response = $this->get(route('labels.create'));
         $response->assertStatus(403);
 
-        $this->signIn();
-
-        $response = $this->get(route('labels.create'));
+        $response = $this->actingAs($this->user)->get(route('labels.create'));
         $response->assertStatus(200);
         $response->assertSeeTextInOrder(
             [
                 __('interface.createLabel')],
             true
         );
+    }
 
+    public function testStore(): void
+    {
         $labelData = [
             'name' => 'Тестовая метка',
             'description' => 'Описание тестовой метки',
         ];
 
-        $response = $this->post(route('labels.store'), $labelData);
+        $response = $this->actingAs($this->user)->post(route('labels.store'), $labelData);
         $response->assertSessionHasNoErrors();
         $response->assertRedirect(route('labels.index'));
         $this->assertDatabaseHas('labels', $labelData);
@@ -97,9 +72,7 @@ class LabelControllerTest extends TestCase
         $response = $this->get(route('labels.edit', ['label' => $this->id]));
         $response->assertStatus(403);
 
-        $this->signIn();
-
-        $response = $this->get(route('labels.edit', ['label' => $this->id]));
+        $response = $this->actingAs($this->user)->get(route('labels.edit', ['label' => $this->id]));
         $response->assertStatus(200);
         $response->assertSeeTextInOrder(
             [
@@ -110,14 +83,12 @@ class LabelControllerTest extends TestCase
 
     public function testUpdate(): void
     {
-        $this->signIn();
-
         $labelData = [
             'name' => 'ОБНОВЛЁННое название задачи',
             'description' => 'новое описание',
         ];
 
-        $response = $this->patch(route('labels.update', ['label' => $this->id]), $labelData);
+        $response = $this->actingAs($this->user)->patch(route('labels.update', ['label' => $this->id]), $labelData);
         $response->assertSessionHasNoErrors();
 
         $updatedLabel = Label::find(1);
@@ -133,9 +104,7 @@ class LabelControllerTest extends TestCase
         $response = $this->delete(route('labels.destroy', ['label' => $labelNotInUseId]));
         $response->assertStatus(403);
 
-        $this->signIn();
-
-        $response = $this->delete(route('labels.destroy', ['label' => $labelNotInUseId]));
+        $response = $this->actingAs($this->user)->delete(route('labels.destroy', ['label' => $labelNotInUseId]));
         $response->assertStatus(302);
         $response->assertRedirect(route('labels.index'));
         $response->assertSessionHasNoErrors();
