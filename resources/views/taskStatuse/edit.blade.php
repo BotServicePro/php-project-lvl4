ab9de0efce491744abf11ae374ec863358f05865
akarakin <bulik889@gmail.com>
2021-11-27 18:25:32 +0200

    changed to fluid interface

--- resources/views/taskStatuse/edit.blade.php
+++ resources/views/taskStatuse/edit.blade.php
@@ -6,9 +6,9 @@
     <main class="container py-4">
         <h2>{{ __('interface.editStatus') }}</h2>
         {{ Form::token() }}
-        {{ Form::model($taskStatus, ['url' => "task_statuses/{$taskStatus['id']}", 'method' => 'PATCH']) }}
+        {{ Form::model($taskStatus, ['url' => "task_statuses/{$taskStatus->id}", 'method' => 'PATCH']) }}
         {{ Form::label('name', __('interface.name')) }}<font style="color: #e3342f">*</font><br>
-        {{ Form::text('name', $taskStatus['name']) }}<br><br>
+        {{ Form::text('name', $taskStatus->name) }}<br><br>
         {{ Form::submit(__('interface.update'), ['class' => 'btn btn-outline-primary mr-2']) }}<br>
     </main>
 @endsection
