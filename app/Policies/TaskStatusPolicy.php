d6ad46b5bd633c966ad22d7ce6795ae08bcf861d
akarakin <bulik889@gmail.com>
2021-12-09 15:14:10 +0200

    Added check for auth

--- app/Policies/TaskStatusPolicy.php
+++ app/Policies/TaskStatusPolicy.php
@@ -15,8 +15,8 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can view any models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function viewAny(?User $user)
     {
@@ -26,20 +26,23 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can view the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param TaskStatus $taskStatus
+     * @return Response|bool
      */
     public function view(User $user, TaskStatus $taskStatus)
     {
+        if (Auth::check()) {
+            return true;
+        }
         return false;
     }
 
     /**
      * Determine whether the user can create models.
      *
-     * @param  \App\Models\User  $user
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @return Response|bool
      */
     public function create(User $user)
     {
@@ -52,9 +55,9 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can update the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param TaskStatus $taskStatus
+     * @return Response|bool
      */
     public function update(User $user, TaskStatus $taskStatus)
     {
@@ -67,9 +70,9 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param TaskStatus $taskStatus
+     * @return Response|bool
      */
     public function delete(User $user, TaskStatus $taskStatus)
     {
@@ -82,9 +85,9 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can restore the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param TaskStatus $taskStatus
+     * @return Response|bool
      */
     public function restore(User $user, TaskStatus $taskStatus)
     {
@@ -97,9 +100,9 @@ class TaskStatusPolicy
     /**
      * Determine whether the user can permanently delete the model.
      *
-     * @param  \App\Models\User  $user
-     * @param  \App\Models\TaskStatus  $taskStatus
-     * @return \Illuminate\Auth\Access\Response|bool
+     * @param User $user
+     * @param TaskStatus $taskStatus
+     * @return Response|bool
      */
     public function forceDelete(User $user, TaskStatus $taskStatus)
     {
